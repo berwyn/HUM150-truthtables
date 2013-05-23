144ea6ab5d02afa44184082c8b04e1a8c6031604
Colten Rouska <crouska@gmail.com>
2013-05-23 17:56:35 -0600

    Added Propositions class

---
+++ HUM150-TruthTables/HUM150-TruthTables/Proposition.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HUM150_TruthTables
+{
+    public class Proposition
+    {
+        public Proposition()
+        {
+            
+        }
+        public void Equals()
+        {
+            
+        }
+        public void Hook()
+        {
+            
+        }
+
+        public void And()
+        {
+            
+        }
+
+        public void Or()
+        {
+            
+        }
+
+        public void NotEqual()
+        {
+            
+        }
+    }
+}
