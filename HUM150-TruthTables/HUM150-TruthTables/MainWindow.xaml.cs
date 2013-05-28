1bd218359f068bb4c8ef79eef079a8c82bdee38e
Joel Alexander <jkalexander67@gmail.com>
2013-05-28 17:11:15 -0600

    Working on fixing truth tables and buggy propositions

--- HUM150-TruthTables/HUM150-TruthTables/MainWindow.xaml.cs
+++ HUM150-TruthTables/HUM150-TruthTables/MainWindow.xaml.cs
@@ -1,63 +1,63 @@
-﻿using MahApps.Metro.Controls;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace HUM150_TruthTables
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : MetroWindow
+    public partial class MainWindow
     {
         private readonly List<Proposition> _propositions = new List<Proposition>(5)
             {
-                new Proposition()
+                new Proposition
                     {
                         Value = false,
                         Definition = ""
                     },
-                    new Proposition()
+                    new Proposition
+                        {
+                        Value = false,
+                        Definition = ""
+                    },
+                    new Proposition
                     {
                         Value = false,
                         Definition = ""
                     },
-                    new Proposition()
+                    new Proposition
                     {
                         Value = false,
                         Definition = ""
                     },
-                    new Proposition()
-                    {
-                        Value = false,
-                        Definition = ""
-                    },
-                    new Proposition()
+                    new Proposition
                     {
                         Value = false,
                         Definition = ""
                     }
             };
-        public List<Proposition> Propositions
+
+        private IEnumerable<Proposition> Propositions
         {
             get { return _propositions; }
         }
 
+        private readonly TruthTable _table;
+
         public MainWindow()
         {
             InitializeComponent();
-            this.PropositionsList.ItemsSource = Propositions;
+            PropositionsList.ItemsSource = Propositions;
+            _table = new TruthTable();
+        }
+
+        private void OnMouseEnter(object sender, MouseEventArgs e)
+        {
+            var count = Propositions.Count(t => !t.Definition.Equals(""));
+            _table.TableCreate(count);
+            Console.WriteLine(count);
         }
     }
 }
--- HUM150-TruthTables/HUM150-TruthTables/TruthTable.cs
+++ HUM150-TruthTables/HUM150-TruthTables/TruthTable.cs
@@ -1,22 +1,76 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Media;
 
 namespace HUM150_TruthTables
 {
     public class TruthTable
     {
-        Proposition[,] Table { get; set; }
+        private const int NumberOfOperations = 6;
+        int[,] Table { get; set; }
+        private FlowDocument flowDoc;
+        private Table table1;
+        private List<string> myChars = new List<string>{"q", "p", "r", "s", "t"}; 
 
-        public Proposition[,] CreateTable(List<Proposition> propositions)
+        public void TableCreate(int count)
         {
-            foreach (var proposition in propositions)
+            flowDoc = new FlowDocument();
+
+            var t = new Table();
+            for (var i = 0; i < 7; i++)
             {
-                
+                t.Columns.Add(new TableColumn());
             }
-            return Table;
+
+            var row = new TableRow {Background = Brushes.Blue, FontSize = 40, FontWeight = FontWeights.Bold};
+
+            row.Cells.Add(new TableCell(new Paragraph(new Run("I span 7 columns"))));
+            row.Cells[0].ColumnSpan = 6;
+
+
+            var rg = new TableRowGroup();
+            rg.Rows.Add(row);
+            t.RowGroups.Add(rg);
+            flowDoc.Blocks.Add(t);
         }
+
+       public void FillWithOnes()
+       {
+           
+
+       }
+        public void RowsWithOneZero(int propositions)
+        {
+            
+        }
+
+        public void RowsWithTwoZeros(int propositions)
+        {
+
+        }
+
+        public void RowsWithThreeZeros(int propositions)
+        {
+
+        }
+
+        public void RowsWithFourZeros(int propositions)
+        {
+
+        }
+
+        public void RowsWithFiveZeros(int propositions)
+        {
+
+        }
+
+        public void RowsWithZeroZeros()
+        {
+
+        }
+
     }
 }
