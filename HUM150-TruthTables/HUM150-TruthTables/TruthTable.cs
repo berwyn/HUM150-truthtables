c04500280a8befda34211018edbb8f5a2661d15a
Joel Alexander <jkalexander67@gmail.com>
2013-05-28 00:25:01 -0600

    Started working on Truth Table class

---
+++ HUM150-TruthTables/HUM150-TruthTables/TruthTable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HUM150_TruthTables
+{
+    public class TruthTable
+    {
+        Proposition[,] Table { get; set; }
+
+        public Proposition[,] CreateTable(List<Proposition> propositions)
+        {
+            foreach (var proposition in propositions)
+            {
+                
+            }
+            return Table;
+        }
+    }
+}
